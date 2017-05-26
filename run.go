8583179b1b51cc09d8db75044c5d6fc50f965766
saumanbiswas <csesauman@gmail.com>
2017-05-26 19:37:34 +0600

    Add Prometheus support (#70)

--- run.go
+++ run.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 
+	acstr "github.com/appscode/go/strings"
 	"github.com/appscode/go/version"
 	"github.com/appscode/log"
 	pcm "github.com/coreos/prometheus-operator/pkg/client/monitoring/v1alpha1"
@@ -11,25 +15,26 @@ import (
 	"github.com/k8sdb/elasticsearch/pkg/controller"
 	"github.com/spf13/cobra"
 	cgcmd "k8s.io/client-go/tools/clientcmd"
+	kapi "k8s.io/kubernetes/pkg/api"
 	clientset "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
 	"k8s.io/kubernetes/pkg/client/unversioned/clientcmd"
 	"k8s.io/kubernetes/pkg/util/runtime"
 )
 
-const (
-	// Default tag
-	canary = "canary"
-)
-
 func NewCmdRun() *cobra.Command {
 	var (
-		masterURL        string
-		kubeconfigPath   string
-		operatorTag      string
-		elasticDumpTag   string
-		governingService string
-		address          string
+		masterURL      string
+		kubeconfigPath string
 	)
+
+	opt := &controller.Option{
+		ElasticDumpTag:    "canary",
+		OperatorTag:       acstr.Val(version.Version.Version, "canary"),
+		ExporterNamespace: namespace(),
+		ExporterTag:       "canary",
+		GoverningService:  "kubedb",
+		Address:           ":8080",
+	}
 
 	cmd := &cobra.Command{
 		Use:   "run",
@@ -41,8 +46,14 @@ func NewCmdRun() *cobra.Command {
 			}
 
 			// Check elasticdump docker image tag
-			if err := docker.CheckDockerImageVersion(controller.ImageElasticDump, elasticDumpTag); err != nil {
-				log.Fatalf(`Image %v:%v not found.`, controller.ImageElasticDump, elasticDumpTag)
+
+			if err := docker.CheckDockerImageVersion(docker.ImageElasticdump, opt.ElasticDumpTag); err != nil {
+				log.Fatalf(`Image %v:%v not found.`, docker.ImageElasticdump, opt.ElasticDumpTag)
+			}
+
+			// Check exporter docker image tag
+			if err := docker.CheckDockerImageVersion(docker.ImageExporter, opt.ExporterTag); err != nil {
+				log.Fatalf(`Image %v:%v not found.`, docker.ImageExporter, opt.ExporterTag)
 			}
 
 			client := clientset.NewForConfigOrDie(config)
@@ -58,24 +69,33 @@ func NewCmdRun() *cobra.Command {
 				log.Fatalln(err)
 			}
 
-			w := controller.New(client, extClient, promClient, operatorTag, elasticDumpTag, governingService, address)
+			w := controller.New(client, extClient, promClient, opt)
 			defer runtime.HandleCrash()
 			fmt.Println("Starting operator...")
 			w.RunAndHold()
 		},
 	}
 
-	operatorVersion := version.Version.Version
-	if operatorVersion == "" {
-		operatorVersion = canary
-	}
-
 	cmd.Flags().StringVar(&masterURL, "master", "", "The address of the Kubernetes API server (overrides any value in kubeconfig)")
 	cmd.Flags().StringVar(&kubeconfigPath, "kubeconfig", "", "Path to kubeconfig file with authorization information (the master location is set by the master flag).")
-	cmd.Flags().StringVar(&operatorTag, "operator", operatorVersion, "Tag of elasticsearch opearator")
-	cmd.Flags().StringVar(&elasticDumpTag, "elasticdump", canary, "Tag of elasticdump")
-	cmd.Flags().StringVar(&governingService, "governing-service", "kubedb", "Governing service for database statefulset")
-	cmd.Flags().StringVar(&address, "address", ":8080", "Address to listen on for web interface and telemetry.")
+	cmd.Flags().StringVar(&opt.ElasticDumpTag, "elasticdump", opt.ElasticDumpTag, "Tag of elasticdump")
+	cmd.Flags().StringVar(&opt.OperatorTag, "operator", opt.OperatorTag, "Tag of elasticsearch opearator")
+	cmd.Flags().StringVar(&opt.ExporterNamespace, "exporter-ns", opt.ExporterNamespace, "Namespace for monitoring exporter")
+	cmd.Flags().StringVar(&opt.ExporterTag, "exporter", opt.ExporterTag, "Tag of monitoring expoter")
+	cmd.Flags().StringVar(&opt.GoverningService, "governing-service", opt.GoverningService, "Governing service for database statefulset")
+	cmd.Flags().StringVar(&opt.Address, "address", opt.Address, "Address to listen on for web interface and telemetry.")
 
 	return cmd
+}
+
+func namespace() string {
+	if ns := os.Getenv("OPERATOR_NAMESPACE"); ns != "" {
+		return ns
+	}
+	if data, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace"); err == nil {
+		if ns := strings.TrimSpace(string(data)); len(ns) > 0 {
+			return ns
+		}
+	}
+	return kapi.NamespaceDefault
 }
