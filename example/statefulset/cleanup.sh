755337ca56f9ef9c5240a3e8349ac90ccb11b742
tamal <tamal@appscode.com>
2017-07-01 10:55:34 -0700

    Examples moved to k8sdb/cli/docs

--- example/statefulset/cleanup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl delete sa governing-elasticsearch
-kubectl delete service elasticsearch-demo
-kubectl delete statefulset kubedb-elasticsearch-demo
--- example/statefulset/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-REPO_ROOT=$GOPATH/src/github.com/k8sdb/elasticsearch
-example=$REPO_ROOT/example/statefulset
-kubectl create -f $example
