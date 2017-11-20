378c27f9b1e552b1cadd2bfbe704bcdb46a3f0e7
Tamal Saha <tamal@appscode.com>
2017-11-19 19:41:11 -0800

    Remove deleted appcode/log package (#99)

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"log"
 
-	logs "github.com/appscode/log/golog"
+	logs "github.com/appscode/go/log/golog"
 	_ "github.com/k8sdb/apimachinery/client/scheme"
 	"github.com/k8sdb/elasticsearch/pkg/cmds"
 )
