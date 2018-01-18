2d38314ec32431076ec40f44aacdb19a6bc7e877
shahriar <aerofoil.kite@gmail.com>
2018-01-18 17:19:13 +0600

    fix defaulting --exporter-tag

--- pkg/cmds/run.go
+++ pkg/cmds/run.go
@@ -47,8 +47,6 @@ func NewCmdRun(version string) *cobra.Command {
 		prometheusCrdKinds = pcm.DefaultCrdKinds
 	)
 
-	opt.Docker.ExporterTag = stringz.Val(version, opt.Docker.ExporterTag)
-
 	cmd := &cobra.Command{
 		Use:               "run",
 		Short:             "Run Elasticsearch in Kubernetes",
@@ -102,7 +100,7 @@ func NewCmdRun(version string) *cobra.Command {
 	cmd.Flags().StringVar(&kubeconfigPath, "kubeconfig", "", "Path to kubeconfig file with authorization information (the master location is set by the master flag).")
 	cmd.Flags().StringVar(&opt.GoverningService, "governing-service", opt.GoverningService, "Governing service for database statefulset")
 	cmd.Flags().StringVar(&opt.Docker.Registry, "docker-registry", opt.Docker.Registry, "User provided docker repository")
-	cmd.Flags().StringVar(&opt.Docker.ExporterTag, "exporter-tag", opt.Docker.ExporterTag, "Tag of kubedb/operator used as exporter")
+	cmd.Flags().StringVar(&opt.Docker.ExporterTag, "exporter-tag", stringz.Val(version, opt.Docker.ExporterTag), "Tag of kubedb/operator used as exporter")
 	cmd.Flags().StringVar(&opt.Address, "address", opt.Address, "Address to listen on for web interface and telemetry.")
 
 	fs := flag.NewFlagSet("prometheus", flag.ExitOnError)
