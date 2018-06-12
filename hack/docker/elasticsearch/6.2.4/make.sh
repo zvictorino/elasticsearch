bc5f9287831ee7002ce650d0e911683ad69477d6
stormmore <stormmore@gmail.com>
2018-06-12 02:45:46 -0700

    Added ES 6.2.4 Docker build files (#142)
    * Added ES 6.2.4 docker setup
    * Updated elasticdump to 3.1.14
    * Updated to search-guard-6

---
+++ hack/docker/elasticsearch/6.2.4/make.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -xeou pipefail
+
+GOPATH=$(go env GOPATH)
+REPO_ROOT="$GOPATH/src/github.com/kubedb/elasticsearch"
+source "$REPO_ROOT/hack/libbuild/common/kubedb_image.sh"
+
+DOCKER_REGISTRY=${DOCKER_REGISTRY:-kubedb}
+IMG=elasticsearch
+TAG=6.2.4
+
+pushd "$REPO_ROOT/hack/docker/elasticsearch/$TAG"
+
+binary_repo $@
---
+++ hack/docker/elasticsearch/6.2/make.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -xeou pipefail
+
+IMG=elasticsearch
+TAG=6.2
+PATCH=6.2.4
+
+docker pull "$DOCKER_REGISTRY/$IMG:$PATCH"
+
+docker tag "$DOCKER_REGISTRY/$IMG:$PATCH" "$DOCKER_REGISTRY/$IMG:$TAG"
+docker push "$DOCKER_REGISTRY/$IMG:$TAG"
