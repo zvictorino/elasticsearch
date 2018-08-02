1c7cb6b1bbba363504c8c1343cdcb036ba74607c
Tamal Saha <tamal@appscode.com>
2018-08-01 18:19:30 -0700

    Revendor api (#160)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/admission/validator_test.go
+++ pkg/admission/validator_test.go
@@ -19,7 +19,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes/fake"
 	clientSetScheme "k8s.io/client-go/kubernetes/scheme"
-	kubeMon "kmodules.xyz/monitoring-agent-api/api"
+	mona "kmodules.xyz/monitoring-agent-api/api/v1"
 )
 
 func init() {
@@ -294,9 +294,9 @@ func editStatus(old api.Elasticsearch) api.Elasticsearch {
 }
 
 func editSpecMonitor(old api.Elasticsearch) api.Elasticsearch {
-	old.Spec.Monitor = &kubeMon.AgentSpec{
-		Agent: kubeMon.AgentPrometheusBuiltin,
-		Prometheus: &kubeMon.PrometheusSpec{
+	old.Spec.Monitor = &mona.AgentSpec{
+		Agent: mona.AgentPrometheusBuiltin,
+		Prometheus: &mona.PrometheusSpec{
 			Port: 1289,
 		},
 	}
@@ -305,8 +305,8 @@ func editSpecMonitor(old api.Elasticsearch) api.Elasticsearch {
 
 // should be failed because more fields required for COreOS Monitoring
 func editSpecInvalidMonitor(old api.Elasticsearch) api.Elasticsearch {
-	old.Spec.Monitor = &kubeMon.AgentSpec{
-		Agent: kubeMon.AgentCoreOSPrometheus,
+	old.Spec.Monitor = &mona.AgentSpec{
+		Agent: mona.AgentCoreOSPrometheus,
 	}
 	return old
 }
